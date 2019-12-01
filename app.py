e5bd1d2f478976e2e2734270bcd07b86d193bd05
Anastasia Tuzhilina <haziran@live.ie>
2019-12-01 22:17:47 +0000

    Styling Our Templates

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def careers():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True )
